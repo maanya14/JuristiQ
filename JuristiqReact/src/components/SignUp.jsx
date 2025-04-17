79e0cdf89fefc627ed8f4821d96e50fa7417e932
Maanya Gupta <157652722+maanya14@users.noreply.github.com>
2025-04-17 23:26:50 +0530

    Update SignUp.jsx

--- JuristiqReact/src/components/SignUp.jsx
+++ JuristiqReact/src/components/SignUp.jsx
@@ -13,14 +13,14 @@ function SignUp() {
   const [otp, setOtp] = useState("")
   const [otpSent, setOtpSent] = useState(false)
   const [otpVerified, setOtpVerified] = useState(false)
-  const API = import.meta.env.REACT_APP_API_URL // if using Vite
+ // const API = import.meta.env.REACT_APP_API_URL // if using Vite
 
   const navigate = useNavigate()
 
   // Send OTP to user's email
   const handleEmailSubmit = async () => {
     try {
-      await axios.post(`${API}/advocate`, { email })
+      await axios.post("https://juristiqbackend.onrender.com/advocate", { email })
       setOtpSent(true)
       alert("OTP sent to your email!")
     } catch (error) {
@@ -32,7 +32,7 @@ function SignUp() {
   // Verify OTP
   const handleOtpSubmit = async () => {
     try {
-      const response = await axios.post(`${API}/verifyotp`, { email, otp })
+      const response = await axios.post("https://juristiqbackend.onrender.com/verifyotp", { email, otp })
       if (response.status === 200) {
         setOtpVerified(true)
         alert("OTP Verified! You can now create a password.")
@@ -53,7 +53,7 @@ function SignUp() {
     }
 
     try {
-      const response = await axios.post(`${API}/register`, {
+      const response = await axios.post("https://juristiqbackend.onrender.com/register", {
         name,
         email,
         password,
