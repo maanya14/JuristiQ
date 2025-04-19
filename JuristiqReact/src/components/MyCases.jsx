1c3879b4f7a290e4757669b251272e1b4231e183
Maanya Gupta <157652722+maanya14@users.noreply.github.com>
2025-04-19 10:56:34 +0530

    Update MyCases.jsx

--- JuristiqReact/src/components/MyCases.jsx
+++ JuristiqReact/src/components/MyCases.jsx
@@ -8,7 +8,7 @@ function MyCases() {
   const [showForm, setShowForm] = useState(false)
   const [cases, setCases] = useState([])
   const [editingCase, setEditingCase] = useState(null) // Track the case being edited
-  const API = import.meta.env.REACT_APP_API_URL // if using Vite
+  //const API = import.meta.env.REACT_APP_API_URL // if using Vite
 
   useEffect(() => {
     fetchCases()
@@ -16,7 +16,7 @@ function MyCases() {
 
   const fetchCases = async () => {
     try {
-      const response = await axios.get(`${API}/getcases`, {
+      const response = await axios.get("https://juristiqbackend.onrender.com/getcases", {
         withCredentials: true, // Ensures cookies are sent
       });
   
@@ -58,10 +58,10 @@ function MyCases() {
     try {
       if (editingCase) {
         // Update Case (PUT request)
-        await axios.put(`${API}/updatecase/${editingCase.case_ref_no}`, newCase)
+        await axios.put(`https://juristiqbackend.onrender.com/updatecase/${editingCase.case_ref_no}`, newCase)
       } else {
         // Add New Case (POST request)
-        await axios.post(`${API}/createcase`, newCase ,{withCredentials: true})
+        await axios.post("https://juristiqbackend.onrender.com/createcase", newCase ,{withCredentials: true})
       }
 
       // Reset the form and fetch updated data
@@ -79,7 +79,7 @@ function MyCases() {
     if (!window.confirm("Are you sure you want to delete this case?")) return
 
     try {
-      await axios.delete(`${API}/deletecase/${case_ref_no}`)
+      await axios.delete(`https://juristiqbackend.onrender.com/deletecase/${case_ref_no}`)
       fetchCases() // Refresh table after delete
     } catch (error) {
       console.error("Error deleting case:", error)
