15b84a92fa8e31ef1c2f3703dcb6df7a03f1de7e
maanya14 <maanya.g14@gmail.com>
2026-06-13 01:08:29 +0530

    mongo

--- autho/db.js
+++ autho/db.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    console.log("Mongo URI:", process.env.MONGODB_URI);
-    await mongoose.connect(process.env.MONGODB_URI, {
+    console.log("Mongo URI:", process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
